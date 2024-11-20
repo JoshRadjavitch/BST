6f57faad9c21b537b9a96dd53c2cbaddfe042ad4
joshradjavitch <joshradjavitch@gmail.com>
2024-11-20 11:45:58 -0500

    Completed Initial Implementation of Binary Search Tree, with a hard coded tree and functional search method

--- BSTJava/BST.java
+++ BSTJava/BST.java
@@ -1,5 +1,47 @@
-package BSTJava;
+import java.util.Scanner;
 
 public class BST {
+
+    private BSTNode root;
+
+    // Constructor
+    BST() {
+        this.root = null;
+    }
+
+    public void buildTree() {
+        root = new BSTNode(5);
+        root.setLeft(new BSTNode(2));
+        root.getLeft().setLeft(new BSTNode(1));
+        root.getLeft().setRight(new BSTNode(3));
+        root.setRight(new BSTNode(12));
+        root.getRight().setLeft(new BSTNode(9));
+        root.getRight().setRight(new BSTNode(21));
+        root.getRight().getRight().setLeft(new BSTNode(19));
+        root.getRight().getRight().setRight(new BSTNode(25));
+    }
+
+    public boolean search(BSTNode root, int value) {
+        if (root == null) return false;
+        if (root.getValue() == value) return true;
+        else {
+            return (search(root.getLeft(), value) || search(root.getRight(), value));
+        }
+    }
     
+    public static void main(String[] args) {
+        BST b = new BST();
+        b.buildTree();
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value to be searched for in the BST: ");
+        int value = in.nextInt();
+        boolean foundVal = b.search(b.root, value);
+
+        if (foundVal) {
+            System.out.println("Value found in BST");
+        } else {
+            System.out.println("Value not found in BST");
+        }
+    }
 }
--- BSTJava/BSTNode.java
+++ BSTJava/BSTNode.java
@@ -1,5 +1,32 @@
-package BSTJava;
-
 public class BSTNode {
+    private int value;
+    private BSTNode left;
+    private BSTNode right;
 
+    // Constructor
+    BSTNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public void setLeft(BSTNode left) {
+        this.left = left;
+    }
+
+    public void setRight(BSTNode right) {
+        this.right = right;
+    }
+
+    public BSTNode getLeft() {
+        return this.left;
+    }
+    
+    public BSTNode getRight() {
+        return this.right;
+    }
+    
+    public int getValue() {
+        return this.value;
+    }
 }
